666989cb245c682a189596ac73c73232a676c4d7
Arfiano Jordhy <arfianoj@gmail.com>
2024-05-26 21:26:48 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <h1 className="text-5xl font-bold w-fit">This is iot men</h1>
+    </div>
+  );
+}
